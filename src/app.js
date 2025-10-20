b72faf1aeb7690e155f2c2132f519298f1b8f39f
rishimanjunath15 <rishimanjunath15@gmail.com>
2025-10-20 08:20:01 +0530

    setup logging and middleware in backend application

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
+  logger.info("hello from acquisitions!");
   res.status(200).send('hello from acquisitions');
 });
 
